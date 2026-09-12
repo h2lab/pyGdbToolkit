40c1e8bf298c307b026bbd2f1a10490ca957ca6b
Philippe Thierry <phil@reseau-libre.net>
2026-09-12 18:33:14 +0200

    fix: fixing redundant naming

--- src/pyGdbToolkit/__init__.py
+++ src/pyGdbToolkit/__init__.py
@@ -2,7 +2,7 @@
 
 from .cmd_faultinfo import FaultInfoCmd
 from .cmd_lscpu import LscpuCmd
-from .cmd_secrethunt import SecretHuntCmd, SecretHuntCmd
+from .cmd_secrethunt import SecretHuntCmd
 
 LscpuCmd()
 FaultInfoCmd()
